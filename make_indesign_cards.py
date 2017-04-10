a40437ccd6058df847f9698e5bab99dd1a77dca3
Jesse Himmelstein <jesse@heavenstone.net>
2017-04-10 18:24:30 +0200

    Adding @ signs for the inddesign cards

--- make_indesign_cards.py
+++ make_indesign_cards.py
@@ -4,10 +4,12 @@ import sys
 reader = csv.reader(sys.stdin)
 writer = csv.writer(sys.stdout)
 
-# Copy header row
-writer.writerow(reader.next())
+# Copy header row, adding @ signs in the middle columns
+header_row = reader.next()
+middle_cols = ["@" + x for x in header_row[1:-1]]
+writer.writerow(header_row[0:1] + middle_cols + header_row[-1:])
 
-# Copy other rows, adding in a .png extension to certain columns
+# Copy other rows, adding in a .png extension to the middle columns
 for row in reader:
 	middle_cols = [x + ".png" for x in row[1:-1]]
 	writer.writerow(row[0:1] + middle_cols + row[-1:])

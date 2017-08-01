c7f20014949d6052287550ab4dcbdb84345a1557
Jesse Himmelstein <jesse@heavenstone.net>
2017-08-01 14:30:36 +0200

    Boat moves around a bit

---
+++ Assets/Scripts/MoveAround.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAround : MonoBehaviour 
+{
+	public GameObject m_thing;
+
+	public float m_distance = 1f;
+	public float m_speed = 0.5f;
+
+	Vector3 m_initialPos;
+	Vector3 m_targetPos;
+
+
+	void Start () 
+	{
+		m_initialPos = m_thing.transform.localPosition;	
+		PickTarget();
+	}
+	
+	void Update () 
+	{
+		if(Vector3.Distance(m_thing.transform.localPosition, m_targetPos) < Mathf.Epsilon)
+		{
+			PickTarget();
+		} 
+		else 
+		{
+			m_thing.transform.localPosition = Vector3.MoveTowards(m_thing.transform.localPosition, m_targetPos, m_speed * Time.deltaTime);
+		}
+	}
+
+	void PickTarget()
+	{
+		m_targetPos = m_initialPos + (Vector3) (m_distance * Random.insideUnitCircle);
+	}
+}

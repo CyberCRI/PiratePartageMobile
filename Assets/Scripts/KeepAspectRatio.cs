64aaf36af5ab7b4a683ce674e26e0b45c48fd082
Jesse Himmelstein <jesse@heavenstone.net>
2017-08-18 09:38:47 +0200

    Fixed aspect ratio

---
+++ Assets/Scripts/KeepAspectRatio.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepAspectRatio : MonoBehaviour {
+	public float m_aspectRatio = 16.0f / 9.0f;
+
+	// Adapted from http://gamedesigntheory.blogspot.fr/2010/09/controlling-aspect-ratio-in-unity.html
+	void Start () 
+	{
+		// determine the game window's current aspect ratio
+		float windowaspect = (float)Screen.width / (float)Screen.height;
+
+		// current viewport height should be scaled by this amount
+		float scaleheight = windowaspect / m_aspectRatio;
+
+		// obtain camera component so we can modify its viewport
+		Camera camera = GetComponent<Camera>();
+
+		// if scaled height is less than current height, add letterbox
+		if (scaleheight < 1.0f)
+		{  
+			Rect rect = camera.rect;
+
+			rect.width = 1.0f;
+			rect.height = scaleheight;
+			rect.x = 0;
+			rect.y = (1.0f - scaleheight) / 2.0f;
+			
+			camera.rect = rect;
+		}
+		else // add pillarbox
+		{
+			float scalewidth = 1.0f / scaleheight;
+
+			Rect rect = camera.rect;
+
+			rect.width = scalewidth;
+			rect.height = 1.0f;
+			rect.x = (1.0f - scalewidth) / 2.0f;
+			rect.y = 0;
+
+			camera.rect = rect;
+		}
+	}
+}
